978510dda595d3ea228db7e34659c58ed031f764
Patryk <pat.sieradzki@gmail.com>
2022-11-16 23:36:26 +0100

    Feat: Added First Service

--- src/app/ui/employee-list/employee-list.component.ts
+++ src/app/ui/employee-list/employee-list.component.ts
@@ -1,11 +1,9 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  ViewEncapsulation,
-} from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { EmployeeService } from '../../services/employee.service';
 import { EmployeeModel } from '../../model/employee.model';
-import { HttpClient } from  "@angular/common/http";
+
 @Component({
   selector: 'employee-list',
   templateUrl: './employee-list.component.html',
@@ -13,7 +11,7 @@ import { HttpClient } from  "@angular/common/http";
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EmployeeListComponent {
-  constructor(private _client: HttpClient) {}
-  data$: Observable<EmployeeModel[] | null> = this._client.get<EmployeeModel[]>('assets/data/employees.json');
+  constructor(private _employeeService: EmployeeService) { }
+  data$: Observable<EmployeeModel[] | null> = this._employeeService.getAll();
 }
 
