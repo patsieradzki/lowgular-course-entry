2be6f7399771c0edd827af6db4a789e17e065cdd
Patryk <pat.sieradzki@gmail.com>
2022-11-16 19:22:42 +0100

    Feat: Added 'email'

---
+++ src/app/ui/employees-faces/employees-faces.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import {Observable} from "rxjs";
+import {EmployeeModel} from "../../model/employee.model";
+
+@Component({
+  selector: 'app-employees-faces',
+  templateUrl: './employees-faces.component.html',
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class EmployeesFacesComponent {
+  data$: Observable<EmployeeModel[]> = this._httpClient.get<EmployeeModel[]>('assets/data/employees.json')
+  constructor(private _httpClient: HttpClient) {}
+
+}
