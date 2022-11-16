a208150786de6ecfb503611cdf19dbd28aa95fe8
Patryk <pat.sieradzki@gmail.com>
2022-11-16 16:59:27 +0100

    Feat: Title added "Lista Pracowników"

--- src/app/ui/employee-list/employee-list.component.ts
+++ src/app/ui/employee-list/employee-list.component.ts
@@ -5,7 +5,7 @@ import {
 } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { EmployeeModel } from '../../model/employee.model';
-
+import { HttpClient } from  "@angular/common/http";
 @Component({
   selector: 'employee-list',
   templateUrl: './employee-list.component.html',
@@ -13,5 +13,7 @@ import { EmployeeModel } from '../../model/employee.model';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EmployeeListComponent {
-  data$: Observable<EmployeeModel[] | null> = of(null);
+  constructor(private _client: HttpClient) {}
+  data$: Observable<EmployeeModel[] | null> = this._client.get<EmployeeModel[]>('assets/data/employees.json');
 }
+
