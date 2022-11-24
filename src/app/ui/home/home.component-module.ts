339543061afffb409e5a6284b883878bf4b187f2
Patryk <pat.sieradzki@gmail.com>
2022-11-24 16:02:45 +0100

    Feat: MOD7EX2 NAVBAR

--- src/app/ui/home/home.component-module.ts
+++ src/app/ui/home/home.component-module.ts
@@ -3,9 +3,10 @@ import { EmployeeListComponentModule } from '../employee-list/employee-list.comp
 import { EmployeeServiceModule } from '../../services/employee.service-module';
 import { EmployeeFormComponentModule } from '../employee-form/employee-form.component-module';
 import { HomeComponent } from './home.component';
+import {RouterModule} from "@angular/router";
 
 @NgModule({
-  imports: [EmployeeListComponentModule, EmployeeServiceModule, EmployeeFormComponentModule],
+  imports: [EmployeeListComponentModule, EmployeeServiceModule, EmployeeFormComponentModule, RouterModule],
   declarations: [HomeComponent],
   providers: [],
   exports: [HomeComponent]
